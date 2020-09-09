1bf36672418d5c8ffba956f2aad8b85eae515cf7
fonsp <fonsvdplas@gmail.com>
2020-09-09 00:52:55 +0000

    🔥

--- warmup.jl
+++ warmup.jl
@@ -1,22 +1,20 @@
 # https://www.youtube.com/watch?v=m2DvmHusyk4
 
-1 + 1
+import Pluto
 
-# import Pluto
+session = Pluto.ServerSession()
 
-# session = Pluto.ServerSession()
+path = tempname()
+original = joinpath(pathof(Pluto) |> dirname |> dirname, "sample", "Tower of Hanoi.jl")
+# so that we don't overwrite the file:
+Pluto.readwrite(original, path)
 
-# path = tempname()
-# original = joinpath(pathof(Pluto) |> dirname |> dirname, "sample", "Tower of Hanoi.jl")
-# # so that we don't overwrite the file:
-# Pluto.readwrite(original, path)
+@info "Loading sample notebook"
+nb = Pluto.load_notebook(Pluto.tamepath(path));
+session.notebooks[nb.notebook_id] = nb;
 
-# @info "Loading sample notebook"
-# nb = Pluto.load_notebook(Pluto.tamepath(path));
-# session.notebooks[nb.notebook_id] = nb;
-
-# @info "Running sample notebook"
-# Pluto.update_save_run!(session, nb, nb.cells; run_async=false, prerender_text=true)
+@info "Running sample notebook"
+Pluto.update_save_run!(session, nb, nb.cells; run_async=false, prerender_text=true)
 
 # nice! we ran the notebook, so we already precompiled a lot
 #= 

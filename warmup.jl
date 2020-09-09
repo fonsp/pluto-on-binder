971208a8d706e712c8a496387bba350c7a8e4032
fonsp <fonsvdplas@gmail.com>
2020-09-09 00:15:06 +0000

    💩

--- warmup.jl
+++ warmup.jl
@@ -9,11 +9,11 @@ original = joinpath(pathof(Pluto) |> dirname |> dirname, "sample", "Tower of Han
 # so that we don't overwrite the file:
 Pluto.readwrite(original, path)
 
-@info "Loading notebook"
+@info "Loading sample notebook"
 nb = Pluto.load_notebook(Pluto.tamepath(path));
 session.notebooks[nb.notebook_id] = nb;
 
-@info "Running notebook"
+@info "Running sample notebook"
 Pluto.update_save_run!(session, nb, nb.cells; run_async=false, prerender_text=true)
 
 # nice! we ran the notebook, so we already precompiled a lot
@@ -22,11 +22,11 @@ Pluto.update_save_run!(session, nb, nb.cells; run_async=false, prerender_text=tr
 # next, we'll run the HTTP server which needs a bit of nasty code
 t = @async Pluto.run(40404; session=session)
 
-sleep(5)
+sleep(3)
 download("http://localhost:40404/")
 
-# this is async because it blocks for some reason
-@async Base.throwto(t, InterruptException())
-sleep(2) # i am pulling these numbers out of thin air
+# lets leave the server running lol
 
-@info "Warmup done"
+# this is async because it blocks for some reason
+# @async Base.throwto(t, InterruptException())
+# sleep(2) # i am pulling these numbers out of thin air

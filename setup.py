f98c15265a097f488df92fbb4055e17b93c72844
Fons van der Plas <fonsvdplas@gmail.com>
2020-12-24 15:42:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setuptools.setup(
           'pluto = plutoserver:setup_plutoserver',
       ]
   },
-  install_requires=['jupyter-server-proxy'],
-  dependency_links=['http://github.com/fonsp/jupyter-server-proxy/tarball/30ab50097369ba07e96c511a1b1649061d85bc18#egg=jupyter-server-proxy']
+  install_requires=['jupyter-server-proxy==1.5.1002'],
+  dependency_links=['http://github.com/fonsp/jupyter-server-proxy/tarball/3a58aa5005f942d0c208eab9a480f6ab171142ef#egg=jupyter-server-proxy']
 )
 
 # because this is a demo of Pluto, we add some popular packages to the global package env and precompile

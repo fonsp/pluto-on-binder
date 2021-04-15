bf0486d10966ed67aefb2267968d1ce5b439fc3b
Fons van der Plas <fonsvdplas@gmail.com>
2021-04-15 23:37:32 +0200

    fix proxy WS issues

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setuptools.setup(
           'pluto = plutoserver:setup_plutoserver',
       ]
   },
-  install_requires=['jupyter-server-proxy'],
+  install_requires=['jupyter-server-proxy==1.5.1002'],
+  dependency_links=['http://github.com/fonsp/jupyter-server-proxy/tarball/3a58aa5005f942d0c208eab9a480f6ab171142ef#egg=jupyter-server-proxy-1.5.1002']
 )
 
 # because this is a demo of Pluto, we add some popular packages to the global package env and precompile

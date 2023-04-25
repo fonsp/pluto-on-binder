6eae450e9b4937ee4eb971f94ea4cec46d61efb9
Fons van der Plas <fonsvdplas@gmail.com>
2023-04-25 18:30:55 +0200

    remove precompilation of Plots, CSV Dataframes during setup
    Because the registry changes so often that it probably invalidates the cache within days

--- setup.py
+++ setup.py
@@ -12,7 +12,3 @@ setuptools.setup(
   },
   install_requires=['jupyter-server-proxy @ git+http://github.com/fonsp/jupyter-server-proxy@3a58aa5005f942d0c208eab9a480f6ab171142ef'],
 )
-
-# because this is a demo of Pluto, we add some popular packages to the global package env and precompile
-import os
-os.system('julia -e "import Pkg; Pkg.add([\\"DataFrames\\", \\"CSV\\", \\"Plots\\"]); Pkg.precompile()"')
